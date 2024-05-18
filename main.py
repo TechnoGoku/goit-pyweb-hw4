627c80837dd98b6530e957c69918eb1ad006c3a8
Ilya <159926377+TechnoGoku@users.noreply.github.com>
2024-05-19 02:17:12 +0300

    update_4

--- main.py
+++ main.py
@@ -19,3 +19,15 @@ class HttpHandler(BaseHTTPRequestHandler):
         self.end_headers()
         with open(filename, 'rb') as fd:
             self.wfile.write(fd.read())
+
+
+def run(server_class=HTTPServer, handler_class=HttpHandler):
+    server_address = ('', 3000)
+    http = server_class(server_address, handler_class)
+    try:
+        http.serve_forever()
+    except KeyboardInterrupt:
+        http.server_close()
+
+if __name__ == '__main__':
+    run()
