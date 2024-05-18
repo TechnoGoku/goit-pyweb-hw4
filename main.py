6f6a9ce7d934f8da18a7e9f3938f6d4eba0b4536
Ilya <159926377+TechnoGoku@users.noreply.github.com>
2024-05-19 02:01:04 +0300

    update_3

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import urllib.parse
+from datetime import datetime
+
 
 class HttpHandler(BaseHTTPRequestHandler):
     def do_GET(self):
+        pr_url = urllib.parse.urlparse(self.path)
+        if pr_url == "/":
+            self.send_html_file("index.html")
+        elif pr_url.path == "/message":
+            self.send_html_file("message.html")
+        else:
+            self.send_html_file("error.html", 404)
+
+
+
+    def send_html_file(self, filename, status=200):
+        self.send_response(status)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        with open(filename, 'rb') as fd:
+            self.wfile.write(fd.read())
+
+
