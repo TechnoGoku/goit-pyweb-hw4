71df0e4cfba9bfcb4e664f437d56b7d5a32072b3
Ilya <159926377+TechnoGoku@users.noreply.github.com>
2024-05-19 02:02:20 +0300

    update_3

--- main.py
+++ main.py
@@ -13,13 +13,9 @@ class HttpHandler(BaseHTTPRequestHandler):
         else:
             self.send_html_file("error.html", 404)
 
-
-
     def send_html_file(self, filename, status=200):
         self.send_response(status)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
         with open(filename, 'rb') as fd:
             self.wfile.write(fd.read())
-
-
