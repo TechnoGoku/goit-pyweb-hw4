5a44013f28e636c0d459d6ffcb001ae7a4155116
Ilya <159926377+TechnoGoku@users.noreply.github.com>
2024-05-19 20:53:26 +0300

    update_18

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 from datetime import datetime
 import logging
 import mimetypes
+import socket
 from threading import Thread
 from pathlib import Path
 from http.server import HTTPServer, BaseHTTPRequestHandler
@@ -11,6 +12,11 @@ import urllib.parse
 
 
 BASE_DIR = Path()
+BUFFER_SIZE = 1024
+HTTP_PORT = 3000
+HTTP_HOST = 'localhost'
+SOCKET_HOST = '127.0.0.1'
+SOCKET_PORT = 5000
 STORAGE_DIR = BASE_DIR / 'storage'
 STORAGE_FILE = STORAGE_DIR / 'data.json'
 
@@ -18,36 +24,10 @@ STORAGE_FILE = STORAGE_DIR / 'data.json'
 class HttpHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         # отримання даних у застосунок з форми
-        data = self.rfile.read(int(self.headers['Content-Length']))
+        size = int(self.headers['Content-Length'])
+        data = self.rfile.read(int(size))
         print(data)
-        # повертаємо дані до початкового вигляду
-        data_parse = urllib.parse.unquote_plus(data.decode())
-        print(data_parse)
-        try:
-            # перетворюємо рядок на словник
-            data_dict = {key: value for key, value in [el.split('=') for el in data_parse.split('&')]}
-            print(data_dict)
 
-            if STORAGE_FILE.exists():
-                try:
-                    with open(STORAGE_FILE, 'r', encoding='utf-8') as file:
-                        existing_data = json.load(file)
-                except json.JSONDecodeError:
-                    logging.error('JSONDecodeError')
-                    existing_data = {}
-            else:
-                existing_data = {}
-
-            timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            existing_data[timestamp] = data_dict
-
-            with open(STORAGE_FILE, 'w', encoding='utf-8') as file:
-                json.dump(existing_data, file, ensure_ascii=False, indent=4)
-
-        except ValueError as err:
-            logging.error(err)
-        except OSError as err:
-            logging.error(err)
         # редірект
         self.send_response(302)
         self.send_header('Location', '/message')
@@ -84,6 +64,37 @@ class HttpHandler(BaseHTTPRequestHandler):
         self.end_headers()
         with open(f".{self.path}", 'rb') as file:
             self.wfile.write(file.read())
+
+
+def save_data_from_form(data):
+    # повертаємо дані до початкового вигляду
+    data_parse = urllib.parse.unquote_plus(data.decode())
+    print(data_parse)
+    try:
+        # перетворюємо рядок на словник
+        data_dict = {key: value for key, value in [el.split('=') for el in data_parse.split('&')]}
+        print(data_dict)
+
+        if STORAGE_FILE.exists():
+            try:
+                with open(STORAGE_FILE, 'r', encoding='utf-8') as file:
+                    existing_data = json.load(file)
+            except json.JSONDecodeError:
+                logging.error('JSONDecodeError')
+                existing_data = {}
+        else:
+            existing_data = {}
+
+        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        existing_data[timestamp] = data_dict
+
+        with open(STORAGE_FILE, 'w', encoding='utf-8') as file:
+            json.dump(existing_data, file, ensure_ascii=False, indent=4)
+
+    except ValueError as err:
+        logging.error(err)
+    except OSError as err:
+        logging.error(err)
 
 
 def run(server_class=HTTPServer, handler_class=HttpHandler):
