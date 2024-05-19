9fe052aff4902e57bcd3f8c515b471eb26678867
Ilya <159926377+TechnoGoku@users.noreply.github.com>
2024-05-19 19:00:10 +0300

    update_14

--- socket_server.py
+++ socket_server.py
@@ -11,6 +11,16 @@ def main():
 
     conn, address = server_socket.accept()
     print(f"Connected by: {address}")
+    while True:
+        msg = conn.recv(1024).decode()
+        if not msg:
+            break
+        print(f"Received by: {msg}")
+        message = input(">>> ")
+        conn.send(message.encode())
+    conn.close()
+    server_socket.close()
+
 
 if __name__ == '__main__':
     main()
