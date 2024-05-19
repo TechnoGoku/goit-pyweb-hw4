c729339ff1770f93ab7bc5885bb5233f1935be4b
Ilya <159926377+TechnoGoku@users.noreply.github.com>
2024-05-19 18:56:16 +0300

    update_13

--- socket_server.py
+++ socket_server.py
@@ -2,8 +2,15 @@ import socket
 
 
 def main():
-    pass
+    host = socket.gethostname()
+    port = 5000
 
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.bind((host, port))
+    server_socket.listen()
+
+    conn, address = server_socket.accept()
+    print(f"Connected by: {address}")
 
 if __name__ == '__main__':
     main()
